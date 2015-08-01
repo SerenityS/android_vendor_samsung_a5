0190cd5cbe0014baa9dce91803472abc0f6727cd
SerenityS <jins4218@naver.com>
2015-08-02 00:36:56 +0900

    fix derp

--- a5-vendor.mk
+++ a5-vendor.mk
@@ -20,7 +20,9 @@ PRODUCT_PACKAGES := \
     LiveWallpapersPicker \
     MagicSmokeWallpapers \
     VisualizationWallpapers \
-    librs_jni
+    librs_jni \
+    libHevcSwDecoder \
+    libtime_genoff
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \
